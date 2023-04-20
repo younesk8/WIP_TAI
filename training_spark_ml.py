5fb9cd0467f1f7acd29f02e448a43915d7007e64
NoobZik <noobzik@pm.me>
2023-04-20 22:59:29 +0200

    Training OK

--- training_spark_ml.py
+++ training_spark_ml.py
@@ -10,12 +10,16 @@ from minio import Minio
 from pyspark import SparkContext
 from pyspark.ml.regression import LinearRegression
 from pyspark.sql import SparkSession
+from pyspark.ml.feature import VectorAssembler
+from pyspark.sql.functions import unix_timestamp
 import minio
 
 
-def create_model(iter, reg, netParam):
+def create_model(iter, features_cols, labelCol):
     # create LinearRegression estimator and set parameters
-    lr = LinearRegression(maxIter=iter, regParam=reg, elasticNetParam=netParam)
+    lr = LinearRegression(maxIter=iter,
+                          featuresCol=features_cols,
+                          labelCol=labelCol)
     return lr
 
 
@@ -32,7 +36,7 @@ def test_model(spark, model):
 
 def save_model(model):
     # Methode pour sauvegarder le modèle vers un lieu spécifié
-    model.save("path_to_save_model")
+    model.save("saved_model/model_test")
 
 
 def main():
@@ -69,24 +73,39 @@ def main():
 
     rdd = spark.sparkContext.parallelize(lines)
     df_spark = spark.read.option("header", True).option("inferSchema", True).csv(rdd)
-    df_spark.show()
+    df_spark = df_spark.dropna()
 
+    df_spark.show()
+    for col in ["order_purchase_timestamp", "order_approved_at", "order_delivered_carrier_date",
+                "order_estimated_delivery_date", "order_delivered_customer_date"]:
+        df_spark = df_spark.withColumn(col + "_int", unix_timestamp(col).cast("int"))
 
     # Diviser le dataset entre entrainement et validation
-    train_data, validation_Data = set_train_and_validation_ds(df_spark, seed)
+    train_data, validation_data = set_train_and_validation_ds(df_spark, seed)
 
     train_data.show()
-    validation_Data.show()
-    lr = create_model(iter=50, reg=0.01, netParam=0.85)
+    validation_data.show()
+
+    features_cols = ["order_purchase_timestamp_int", "order_approved_at_int",
+                     "order_delivered_carrier_date_int", "order_estimated_delivery_date_int"]
+    target_col = "order_delivered_customer_date_int"
+
+    assembler = VectorAssembler(inputCols=features_cols, outputCol="features")
+
+    # transform the data using the VectorAssembler
+    data_with_features = assembler.transform(df_spark)
+
+    lr = create_model(iter=500, features_cols="features", labelCol=target_col)
 
     # fit the model to the data
-    model = lr.fit(train_data)
+    model = lr.fit(data_with_features)
 
     # print the model coefficients and intercept
     print("Coefficients: " + str(model.coefficients))
     print("Intercept: " + str(model.intercept))
 
     # save the model
+    save_model(model)
 
     # evaluate the model on test data
 
